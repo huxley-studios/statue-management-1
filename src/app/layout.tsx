e56e05cea74da5293cccd7570404238873369972
huxley-studios <alan@huxleystudios.com.au>
2025-01-25 06:48:53 +1100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default async function RootLayout({
   children,
 }) {
-  const session = await getServerSession()
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
