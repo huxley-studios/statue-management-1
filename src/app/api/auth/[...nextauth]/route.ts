07888debd1a50b1b5f5c5aab5fd32c20ed2404e2
huxley-studios <alan@huxleystudios.com.au>
2025-01-25 06:54:22 +1100

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth/next"
+import { GoogleProvider } from "@auth/core/providers/google"
 
 const handler = NextAuth({
   providers: [
