bd0d6aef4d2a73a4584cd95e246c63977bec3328
huxley-studios <alan@huxleystudios.com.au>
2025-01-25 11:21:38 +1100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default async function RootLayout({
+export default function RootLayout({
   children,
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
