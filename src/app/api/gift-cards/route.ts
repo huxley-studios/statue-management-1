77101756117ee6d53a1ef64b58e2ed3724154072
huxley-studios <alan@huxleystudios.com.au>
2025-01-25 16:50:04 +1100

    Update route.ts

--- src/app/api/gift-cards/route.ts
+++ src/app/api/gift-cards/route.ts
@@ -1,28 +1,26 @@
-import { NextResponse, NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 import connectDB from '@/lib/mongodb'
 import GiftCard from '@/models/GiftCard'
 
 export async function GET(
   request: Request,
-  context: { params: { code: string } }
+  { params }: { params: { code: string } } & { searchParams: URLSearchParams }
 ) {
   await connectDB()
-  const giftCard = await GiftCard.findOne({ 
-    activationCode: context.params.code 
-  })
+  const giftCard = await GiftCard.findOne({ activationCode: params.code })
   return NextResponse.json(giftCard)
 }
 
 export async function PUT(
- request: NextRequest, 
- { params }: { params: { code: string } }
+  request: Request,
+  { params }: { params: { code: string } } & { searchParams: URLSearchParams }
 ) {
- await connectDB()
- const data = await request.json()
- const giftCard = await GiftCard.findOneAndUpdate(
-   { activationCode: params.code },
-   data,
-   { new: true }
- )
- return NextResponse.json(giftCard)
+  await connectDB()
+  const data = await request.json()
+  const giftCard = await GiftCard.findOneAndUpdate(
+    { activationCode: params.code },
+    data,
+    { new: true }
+  )
+  return NextResponse.json(giftCard)
 }
